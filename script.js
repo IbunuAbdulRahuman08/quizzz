5f3a6d61c70d2d64e1d4eead74de20425d5314b0
IbunuAbdulRahuman08 <ibunuiar082000@gmail.com>
2024-05-01 21:42:30 +0530

    update

---
+++ script.js
@@ -0,0 +1,39 @@
+var inputBox = document.getElementById("input-box");
+var listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        var li = document.createElement("li");
+        li.textContent = inputBox.value;
+        listContainer.appendChild(li);
+        var span =document.createElement("span");
+        span.innerHTML= "\u00d7";
+        li.appendChild(span)
+        inputBox.value = ''; // Clear the input box after adding the task
+        saveData();
+    }
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+
+}
+showTask();
